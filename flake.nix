577dfae3c4b8477e6165bf677f15628a1bcaf9e4
Seiju Berg <flyrhinosfly@gmail.com>
2025-08-24 19:15:30 -0400

    refactored flake to expose binary better

--- flake.nix
+++ flake.nix
@@ -11,21 +11,25 @@
       bin_name = "scp";
       version = "0.0.0";
     in {
-      packages.${system}.default = pkgs.python3Packages.buildPythonPackage {
-        pname = program_name;
-        version = version;
-        format = "pyproject";
-        src = ./.;
+      packages.${system} = {
+        stack-copy = pkgs.python3Packages.buildPythonPackage {
+          pname = program_name;
+          version = version;
+          format = "pyproject";
+          src = ./.;
 
-        # Needed for PEP 517/518 projects
-        nativeBuildInputs =
-          [ pkgs.python3Packages.setuptools pkgs.python3Packages.wheel ];
+          # Needed for PEP 517/518 projects
+          nativeBuildInputs =
+            [ pkgs.python3Packages.setuptools pkgs.python3Packages.wheel ];
+        };
+
+        default = self.packages.${system}.stack-copy;
       };
 
       # Make it runnable via `nix run github:you/mytool`
       apps.${system}.default = {
         type = "app";
-        program = "${self.packages.${system}.default}/bin/${bin_name}";
+        program = "${self.packages.${system}.stack-copy}/bin/${bin_name}";
       };
 
       # Development shell with your program installed
